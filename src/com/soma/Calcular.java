45ffdf8a0de9b50e47d7ccc2a489c7dbc9cf5436
AliciaViana <aliciasouza601@gmail.com>
2022-03-17 00:03:46 -0300

    reorganizado codigo

---
+++ src/com/soma/Calcular.java
@@ -0,0 +1,8 @@
+package com.soma;
+
+public class Calcular {
+
+	public int realiazaSoma(int numero1, int numero2) {
+		return (numero1 + numero2);
+	}
+}
--- src/com/soma/NumberS.java
+++ src/com/soma/NumberS.java
@@ -6,19 +6,17 @@ public class NumberS {
 
 	public static void main(String[] args) {
 
-		ApoioSoma numero = new ApoioSoma();
+		Calcular calcular = new Calcular();
 		Scanner myObj = new Scanner(System.in);
-		// int a, b, c, =0;
 
 		System.out.println("Insira um numero: ");
-		int somatorio = myObj.nextInt();
+		int numero1 = myObj.nextInt();
 
 		System.out.println("Insira outro numero: ");
-		int carregaSegundaSoma = myObj.nextInt();
+		int numero2 = myObj.nextInt();
 
-		int valorTotal = numero.carregaTotal(somatorio);
+		int valorTotal = calcular.realiazaSoma(numero1, numero2);
 		System.out.println("Sua soma é: " + valorTotal);
-
 	}
 
 }
