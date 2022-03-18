8ed919619a6ba4d0f79248d48d713f5a61e1f3d8
AliciaViana <aliciasouza601@gmail.com>
2022-03-17 21:23:58 -0300

    metodo adiconado; close #1

--- src/com/soma/Calcular.java
+++ src/com/soma/Calcular.java
@@ -5,4 +5,9 @@ public class Calcular {
 	public int realiazaSoma(int numero1, int numero2) {
 		return (numero1 + numero2);
 	}
+
+	public int realiazaMultiplicacao(int numero3, int numero4) {
+		return (numero3 * numero4);
+
+	}
 }
--- src/com/soma/NumberS.java
+++ src/com/soma/NumberS.java
@@ -17,6 +17,16 @@ public class NumberS {
 
 		int valorTotal = calcular.realiazaSoma(numero1, numero2);
 		System.out.println("Sua soma é: " + valorTotal);
+
+		System.out.println("Insira um numero para multiplicar: ");
+		int numero3 = myObj.nextInt();
+
+		System.out.println("Insira mais um multiplicador: ");
+		int numero4 = myObj.nextInt();
+
+		int valorTotalMulti = calcular.realiazaMultiplicacao(numero3, numero4);
+		System.out.println(" Sua multiplicação é: " + valorTotalMulti);
+
 	}
 
 }
